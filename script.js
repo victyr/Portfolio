7b6140d52bc2987cf6fb0c1b1b492b6b6dabda09
victyr <45426661+victyr@users.noreply.github.com>
2022-06-14 01:29:59 +0300

    Add java script menu and hamburger

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click",() => {
+   hamburger.classList.toggle("active");
+   navmenu.classList.toggle("active");
+})
+
+document.querySelectorAll()
