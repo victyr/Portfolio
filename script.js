381d207bc265d697a738dfe140c281c49d8d0c83
victyr <45426661+victyr@users.noreply.github.com>
2022-06-14 22:48:05 +0300

    node j

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function openNav() {
-  document.getElementById('myNav').style.width = "100%";
+
+export default function openNav() {
+  document.getElementById('myNav').style.width = '100%';
 }
 
-function closeNav() {
-  document.getElementById('myNav').style.width = "0%";
+export default function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
 }
