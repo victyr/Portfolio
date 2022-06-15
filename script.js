69c812dffb7dd9a012e57cf6bd27a177a7369a10
victyr <45426661+victyr@users.noreply.github.com>
2022-06-15 13:44:47 +0300

    Add CSS

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const containMenu = document.getElementById('container_menu');
 const hamburgerMenu = document.getElementById('hamburger_menu');
+const closeSidebar = document.querySelector('.sidebar_menu');
 const closeButton = document.getElementById('close_button');
 const navElement = document.querySelectorAll('.menu_mobile a');
 
 hamburgerMenu.addEventListener('click', () => {
   containMenu.style.display = 'flex';
+});
+
+closeSidebar.addEventListener('click', () => {
+  containMenu.style.display = 'none';
 });
 
 closeButton.addEventListener('click', () => {
